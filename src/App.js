29d4688e274053454e711d0c959e4f8c0f830adc
Parshuram12345 <112482254+Parshuram12345@users.noreply.github.com>
2022-08-30 17:41:31 +0530

    initial commit
    form ui with database authentication

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Maincomponent from './main_component/MainComponent'
+
+function App() {
+  return (
+    <div>
+        <Maincomponent/>
+    </div>
+  )
+}
+
+export default App
